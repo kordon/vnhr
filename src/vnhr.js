de55a12016c628982e0dcd37045bed6912d6d84c
Sérgio Ramos <mail@sergioramos.me>
2013-10-21 22:17:58 +0100

    new ring structure

--- src/vnhr.js
+++ src/vnhr.js
@@ -4,71 +4,155 @@ var type = require('type-component'),
     sha1 = require('sha1')
 
 
+/**
+ * **Virtual Node Hash Ring**
+ *
+ * With array:
+ * ```javascript
+ * var hashring = vnhr([
+ *   '192.168.0.102:11212',
+ *   '192.168.0.103:11213',
+ *   '192.168.0.104:11214'
+ * ], {
+ *   replicas: 3,
+ *   vnodes: 1024
+ * })
+ * ```
+ *
+ * With only one pnode:
+ * ```javascript
+ * var hashring = vnhr('192.168.0.102:11212', {
+ *   replicas: 3,
+ *   vnodes: 1024
+ * })
+ * ```
+ *
+ * With no pnodes or options:
+ * ```javascript
+ * var hashring = vnhr()
+ * ```
+ *
+ * @param {array|string} [pnodes]
+ * @param {object} [options]
+ * @access public
+ */
 var vnhr = module.exports = function (opts) {
-  if(!(this instanceof vnhr)) return new vnhr(utils.args.apply(null, arguments))
-  
-  this.replicas = opts.options.replicas
-  this.ring = utils.array(opts.options.vnodes)
-  this.metadata = Object()
+  if(!(this instanceof vnhr))
+    return new vnhr(utils.args.apply(null, arguments))
 
-  opts.servers.forEach(this.push, this)
+  // number of replicas that each key has (by default)
+  this.replicas = opts.options.replicas
+  // ring with `opts.options.vnodes` size
+  this.ring = utils.array(opts.options.vnodes)
+  // metadata for each pnode
+  this.pnodes = {}
+
+  // add pnodes to the ring
+  this.push(opts.servers)
 }
 
 require('util').inherits(vnhr, require('events').EventEmitter)
 
+/**
+ * Add a new pnode
+ *
+ * ```javascript
+ * vnhr().push('192.168.0.102:11212')
+ * ```
+ *
+ * ```javascript
+ * vnhr().push([
+ *   '192.168.0.102:11212',
+ *   '192.168.0.103:11213',
+ *   '192.168.0.104:11214'
+ * ])
+ * ```
+ *
+ * @param {array|string} [pnodes]
+ * @param {object} [options]
+ * @access public
+ */
 vnhr.prototype.push = function (server) {
+  if(Array.isArray(server)) return server.forEach(this.push, this)
+
   server = utils.metadata(server)
-  
-  if(Object.keys(this.metadata).indexOf(server.id) >= 0) return false
+
+  // the server was already added
+  if(Object.keys(this.pnodes).indexOf(server.id) >= 0)
+    return false
+
+  // the ring has no space
+  if(Object.keys(this.pnodes).length >= this.ring.length)
+    return false
+
   var before = JSON.parse(JSON.stringify(this.ring))
-  
-  this.metadata[server.id] = server
-  var pnodes = Object.keys(this.metadata).sort()
-  
+
+  this.pnodes[server.id] = server
+  var pnodes = Object.keys(this.pnodes).sort()
+
   this.ring = this.ring.map(function (server, i) {
-    return pnodes[i % Object.keys(this.metadata).length]
+    return {
+      pnode: pnodes[i % Object.keys(this.pnodes).length],
+      vnode: i + 1
+    }
   }, this)
-  
+
   var after = JSON.parse(JSON.stringify(this.ring))
+
   this.propagate(before, after)
 }
 
+/**
+ * Propagate ring changes
+ *
+ * @param {array} before
+ * @param {array} after
+ * @access private
+ */
 vnhr.prototype.propagate = function (before, after) {
-  if(before.length > after.length) before.forEach(function (vnode, i) {
-    if(i < after.length) return
-    
-    this.emit('rm', this.metadata[vnode], i)
-  }, this)
-  
-  before.forEach(function (vnode, i) {
-    this.emit('handoff', this.metadata[vnode], this.metadata[after[i]], i)
-  }, this)
-  
-  if(before.length < after.length) after.forEach(function (vnode, i) {
-    if(i < before.length) return
-    
-    this.emit('add', this.metadata[vnode], i)
+  before.forEach(function (vpnode, i) {
+    var to = JSON.parse(JSON.stringify(this.pnodes[after[i].pnode]))
+    var from = null
+
+    if(vpnode)
+      from = JSON.parse(JSON.stringify(this.pnodes[vpnode.pnode]))
+
+    if(from && from.name === to.name)
+      return
+
+    this.emit('handoff', from, to, vpnode.vnode)
   }, this)
 }
 
+/**
+ * Get vnodes for key
+ *
+ * @param {string} key
+ * @param {number} [count=this.replicas]
+ * @param {array}
+ * @access public
+ */
 vnhr.prototype.get = function (key, count) {
-  var pnodes = Object.keys(this.metadata)
-  
-  if(!pnodes.length) return false
-  if(type(count) !== 'number') count = this.replicas
+  var pnodes = Object.keys(this.pnodes)
+
+  // no pnodes available
+  if(!pnodes.length)
+    return false
+
+  // if no count is defined,
+  if(type(count) !== 'number')
+    count = this.replicas
   if(this.replicas < pnodes.length) count = pnodes.length - 1
-  
+
   key = sha1(key)
   var num = hex.encode(key).substring(0, 8)
   var vnode = num % this.ring.length
 
-  return this.ring.map(function (pnode, vnode) {
-    return [pnode, vnode]
-  }).splice(vnode - count + 1, count).map(function (vpnode) {
+  return utils.slice(this.ring, vnode, count).map(function (vpnode) {
     return {
-      pnode: this.metadata[vpnode[0]],
-      vnode: vpnode[1],
-      main: vpnode[1] === vnode
+      vnode: vpnode.vnode,
+      pnode: this.pnodes[vpnode.pnode],
+      main: vpnode.vnode === vnode
     }
   }, this)
 }
--- test/index.js
+++ test/index.js
@@ -1,21 +1,88 @@
 var vnhr = process.env.VNHR_COV ? require('../lib-cov/vnhr') : require('../'),
+    utils = process.env.VNHR_COV ? require('../lib-cov/utils') : require('../src/utils'),
     assert = require('assert')
+
+suite('utils')
+
+test('array', function () {
+  var a = utils.array(100).map(function (n, i) {
+    return i
+  })
+
+  a.forEach(function (n, i) {
+    assert(n === i)
+  })
+
+  assert(a.length === 100)
+})
+
+test('slice', function () {
+  // [1, 2, 3, 4, 5]
+  var array = utils.array(5).map(function (n, i) {
+    return i + 1
+  })
+
+  // main 0
+  // [5, 1, 2]
+  var a = utils.slice(array, 0, 3)
+  assert(a.length === 3)
+
+  assert(a[0] === 5)
+  assert(a[1] === 1)
+  assert(a[2] === 2)
+
+  // main 1
+  // [1, 2, 3]
+  a = utils.slice(array, 1, 3)
+  assert(a.length === 3)
+
+  assert(a[0] === 1)
+  assert(a[1] === 2)
+  assert(a[2] === 3)
+
+  // main 2
+  // [2, 3, 4]
+  a = utils.slice(array, 2, 3)
+  assert(a.length === 3)
+
+  assert(a[0] === 2)
+  assert(a[1] === 3)
+  assert(a[2] === 4)
+
+  // main 3
+  // [3, 4, 5]
+  a = utils.slice(array, 3, 3)
+  assert(a.length === 3)
+
+  assert(a[0] === 3)
+  assert(a[1] === 4)
+  assert(a[2] === 5)
+
+  // main 4
+  // [4, 5, 1]
+  a = utils.slice(array, 4, 3)
+  assert(a.length === 3)
+
+  assert(a[0] === 4)
+  assert(a[1] === 5)
+  assert(a[2] === 1)
+})
 
 suite('api')
 
 test('construct with a string', function () {
   var hr = vnhr('192.168.0.102:11212')
 
-  assert(Object.keys(hr.metadata).length === 1)
+  assert(Object.keys(hr.pnodes).length === 1)
   assert(hr.replicas === 3)
   assert(hr.ring.length === 1024)
-  assert(Object.keys(hr.metadata).length === 1)
-  
-  var metadata = Object.keys(hr.metadata).map(function (key) {
-    return hr.metadata[key]
+  assert(Object.keys(hr.pnodes).length === 1)
+
+  var pnodes = Object.keys(hr.pnodes).map(function (key) {
+    return hr.pnodes[key]
   }).pop()
-  
-  assert(metadata.name === '192.168.0.102:11212')
+
+  assert(pnodes.name === '192.168.0.102:11212')
 })
 
 test('construct with an array', function () {
@@ -24,17 +91,17 @@ test('construct with an array', function () {
     '192.168.0.103:11212',
     '192.168.0.104:11212'
   ]
-  
+
   var hr = vnhr(pnodes)
 
   assert(hr.replicas === 3)
   assert(hr.ring.length === 1024)
-  assert(Object.keys(hr.metadata).length === 3)
+  assert(Object.keys(hr.pnodes).length === 3)
 
-  assert(Object.keys(hr.metadata).every(function (key) {
-    var pnode = hr.metadata[key]
+  assert(Object.keys(hr.pnodes).every(function (key) {
+    var pnode = hr.pnodes[key]
     var asserted = true
-    
+
     asserted = asserted && pnodes.indexOf(pnode.name) >= 0
     return asserted
   }))
@@ -42,8 +109,8 @@ test('construct with an array', function () {
 
 test('construct with no arguments', function () {
   var hr = vnhr()
-  
-  assert(Object.keys(hr.metadata).length === 0)
+
+  assert(Object.keys(hr.pnodes).length === 0)
   assert(hr.ring.every(function(vnode) {
     return !vnode
   }))
@@ -53,10 +120,10 @@ test('add server after zero-argument constructor', function () {
   var server = '192.168.0.102:11212'
   var hr = vnhr()
   hr.push(server)
-  
-  assert(Object.keys(hr.metadata).length === 1)
-  assert(hr.ring.every(function(vnode) {
-    return hr.metadata[vnode].name === server
+
+  assert(Object.keys(hr.pnodes).length === 1)
+  assert(hr.ring.every(function(vpnode) {
+    return hr.pnodes[vpnode.pnode].name === server
   }))
 })
 
@@ -66,48 +133,46 @@ test('key lookup', function () {
     '192.168.0.103:11213',
     '192.168.0.104:11214'
   ]
-  
+
   var hr = vnhr(pnodes)
-  
+
   var bar = {
     vpnodes: hr.get('bar')
   }
-  
+
   var foo = {
     vpnodes: hr.get('foo')
-  }  
-  
+  }
+
   bar.names = bar.vpnodes.map(function (result) {
     return result.pnode.name
   })
-  
+
   bar.main = bar.vpnodes.filter(function (result) {
     return result.main
   }).map(function (vpnode) {
     return vpnode.pnode
   }).shift()
-  
+
   foo.names = foo.vpnodes.map(function (result) {
     return result.pnode.name
   })
-  
+
   foo.main = foo.vpnodes.filter(function (result) {
     return result.main
   }).map(function (vpnode) {
     return vpnode.pnode
   }).shift()
-  
-  
+
   assert(foo.names.length === 3)
   assert(foo.names.indexOf(pnodes[0]) >= 0)
   assert(foo.names.indexOf(pnodes[1]) >= 0)
   assert(foo.names.indexOf(pnodes[2]) >= 0)
-  
+
   assert(bar.names.length === 3)
   assert(bar.names.indexOf(pnodes[0]) >= 0)
   assert(bar.names.indexOf(pnodes[1]) >= 0)
   assert(bar.names.indexOf(pnodes[2]) >= 0)
-  
   assert(bar.main.name !== foo.main.name)
 })
 
@@ -117,31 +182,87 @@ test('even distribution', function () {
     '192.168.0.103:11213',
     '192.168.0.104:11214'
   ])
-  
+
   var servers = []
 
   hr.ring.forEach((function () {
     var bef, prev
-    
-    return function (vnode, i) {
-      var name = hr.metadata[vnode].name
-      
+
+    return function (vpnode, i) {
+      var name = hr.pnodes[vpnode.pnode].name
+
       if(!bef) {
         bef = name
         return
       }
-      
+
       if(!prev) {
         prev = name
         return
       }
-      
+
       assert(name !== bef)
       assert(name !== prev)
       assert(prev !== bef)
-      
+
       bef = prev
       prev = name
     }
   })())
 })
+
+test('consistent results', function () {
+  var pnodes = [
+    '192.168.0.102:11212',
+    '192.168.0.104:11214'
+  ]
+
+  var hr = vnhr(pnodes)
+
+  var bar1 = {
+    vpnodes: hr.get('bar')
+  }
+
+  hr.push('192.168.0.103:11213')
+
+  var bar2 = {
+    vpnodes: hr.get('bar')
+  }
+
+  bar1.names = bar1.vpnodes.map(function (result) {
+    return result.pnode.name
+  })
+
+  bar1.main = bar1.vpnodes.filter(function (result) {
+    return result.main
+  }).map(function (vpnode) {
+    return vpnode.pnode
+  }).shift()
+
+  bar2.names = bar2.vpnodes.map(function (result) {
+    return result.pnode.name
+  })
+
+  bar2.main = bar2.vpnodes.filter(function (result) {
+    return result.main
+  }).map(function (vpnode) {
+    return vpnode.pnode
+  }).shift()
+
+  assert(bar1.names.length === bar2.names.length)
+  assert(bar1.names.indexOf(pnodes[0]) === bar2.names.indexOf(pnodes[0]))
+  assert(bar1.names.indexOf(pnodes[1]) === bar2.names.indexOf(pnodes[1]))
+  assert(bar1.names.indexOf(pnodes[2]) === bar2.names.indexOf(pnodes[2]))
+  assert(bar1.main.name === bar2.main.name)
+})
+
+// test('handoff event', function (done) {
+//   var my_server = '192.168.0.103:11213'
+//   var hr = vnhr([my_server, '192.168.0.104:11214'])
+// 
+//   hr.on('handoff', function (from, to) {
+//     if(from.name !== my_server || to.name !== my_server) return
+//   })
+// 
+//   hr.push('192.168.0.102:11212')
+// })
